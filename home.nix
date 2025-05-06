9946d5f16678889ccc007fc18e455f4ff3286ba4
Michi Tanaka <tanaka.michi@icloud.com>
2025-05-06 13:43:22 -0700

    added some packages to test

--- home.nix
+++ home.nix
@@ -1,11 +1,18 @@
-{ ... }: {
+{ pkgs, ... }: {
     # This is required information for home-manager to do its job
     home = {
         stateVersion = "23.11";
         username = "michitanaka";
         homeDirectory = "/Users/michitanaka";
-        packages = [ ];
+        packages = [
+            pkgs.git
+            pkgs.neovim
+        ];
     };
+
+    # Ensure programs are using ~/.config rather than /Users/michitanaka/Library/...
+    xdg.enable = true;
+
     programs.home-manager.enable = true;
     programs.fish.enable = true;
 }
