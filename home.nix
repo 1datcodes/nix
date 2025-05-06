cc3c33638d9fa5a7c8c1d7717e971c68cff5c358
Michi Tanaka <tanaka.michi@icloud.com>
2025-05-06 13:38:54 -0700

    fixed typo

--- home.nix
+++ home.nix
@@ -1,7 +1,7 @@
 { ... }: {
     # This is required information for home-manager to do its job
     home = {
-        stateVerision = "23.11";
+        stateVersion = "23.11";
         username = "michitanaka";
         homeDirectory = "/Users/michitanaka";
         packages = [ ];
