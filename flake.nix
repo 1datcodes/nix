f41a714a0a51dad91e5fe2d54f53dc3e84129a62
1datcodes <tanakamichi7@gmail.com>
2025-05-07 00:57:58 -0700

    configured mac according to Dreams of Autonomy

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Zenful nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, mac-app-util, nix-homebrew, nixpkgs }:
+  let
+    configuration = { pkgs, config, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      nixpkgs.config.allowUnfree = true;
+      environment.systemPackages =
+        [ 
+	  pkgs.git
+	  pkgs.vscode
+	  pkgs.neovim
+        ];
+
+	homebrew = {
+		enable = true;
+		brews = [
+			"mas"
+		];
+		casks = [
+			"brave-browser"
+			"1password@7"
+		];
+		masApps = {
+		};
+		onActivation.cleanup = "zap";
+	};
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+      	configuration
+	mac-app-util.darwinModules.default
+	nix-homebrew.darwinModules.nix-homebrew
+	{
+		nix-homebrew = {
+			enable = true;
+			enableRosetta = true;
+			user = "michitanaka";
+		};
+	}
+	];
+    };
+  };
+}
