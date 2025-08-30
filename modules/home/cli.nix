3cca6076db5989ed2a1e269224c4dde8d1dbf1ef
1datcodes <tanakamichi7@gmail.com>
2025-08-29 18:21:25 -0700

    added caligula, iso burning tool

--- modules/home/cli.nix
+++ modules/home/cli.nix
@@ -1,32 +1,32 @@
-{ config, inputs, pkgs, ... }:
-{
-	home.packages = with pkgs; [
-		git
-		git-credential-manager
-		git-lfs
-		fastfetch
-		htop
-		btop
-		starship
-		ollama
-		radeontop
-		pciutils
-		usbutils
-		tree
+{ config, inputs, pkgs, ... }: {
+  home.packages = with pkgs; [
+    git
+    git-credential-manager
+    git-lfs
+    fastfetch
+    htop
+    btop
+    starship
+    ollama
+    radeontop
+    pciutils
+    usbutils
+    tree
     wl-clipboard
-	];
+    caligula
+  ];
 
-	programs.git = {
-		enable = true;
-		userName = "1datcodes";
-		userEmail = "tanakamichi7@gmail.com";
+  programs.git = {
+    enable = true;
+    userName = "1datcodes";
+    userEmail = "tanakamichi7@gmail.com";
 
-		lfs.enable = true;
-	
-		extraConfig = {
-			credential.helper = "manager";
-			credential."https://github.com".username = "1datcodes";
-			credential.credentialStore = "cache";
-		};
-	};
+    lfs.enable = true;
+
+    extraConfig = {
+      credential.helper = "manager";
+      credential."https://github.com".username = "1datcodes";
+      credential.credentialStore = "cache";
+    };
+  };
 }
