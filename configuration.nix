2f5e86c7b2d0ff119c2dc422644d15f3e89a2e7d
1datcodes <tanakamichi7@gmail.com>
2025-08-30 23:49:23 -0700

    changed hostname

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "IntelMBP"; # Define your hostname.
+  networking.hostName = "intelmbp"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
