3bf890e2bb9f6431c7930da8093b96cb4a8f24be
1datcodes <tanakamichi7@gmail.com>
2025-09-04 12:58:45 -0700

    syntax fix

--- flake.nix
+++ flake.nix
@@ -1,44 +1,45 @@
 {
-	description = "Simple NixOS Flake";
+  description = "Simple NixOS Flake";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		catppuccin.url = "github:catppuccin/nix";
-		hyprland.url = "github:hyprwm/Hyprland";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    catppuccin.url = "github:catppuccin/nix";
+    hyprland.url = "github:hyprwm/Hyprland";
     quickshell = {
       url = "github:quickshell-mirror/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
-	};
+    };
+  };
 
-	outputs = { self, nixpkgs, home-manager, catppuccin, hyprland, quickshell, ... }@inputs: {
-		nixosConfigurations = {
-			pro = nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
-				specialArgs = { inherit inputs; };
-				modules = [
-					./hosts/pro/configuration.nix
-					catppuccin.nixosModules.catppuccin
+  outputs = { self, nixpkgs, home-manager, catppuccin, hyprland, quickshell, ...
+    }@inputs: {
+      nixosConfigurations = {
+        pro = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/pro/configuration.nix
+            catppuccin.nixosModules.catppuccin
 
-					home-manager.nixosModules.home-manager
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
 
-						home-manager.users.michitanaka = {
-							imports = [
-								./home/michitanaka/home.nix
-								catppuccin.homeModules.catppuccin
-							];
-						};
-					}
-				];
-			};
-		};
-	};
+              home-manager.users.michitanaka = {
+                imports = [
+                  ./home/michitanaka/home.nix
+                  catppuccin.homeModules.catppuccin
+                ];
+              };
+            }
+          ];
+        };
+      };
+    };
 }
 
